73d0158ece6f25a70f856dcb9c5d49fbc2297364
volbok <59297702+volbok@users.noreply.github.com>
2023-02-09 16:29:23 -0300

    Update server.js

--- server.js
+++ server.js
@@ -14,10 +14,6 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Request-Private-Network", "false");
   res.header("Access-Control-Allow-Private-Network", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
   next();
 });
 app.use(bodyParser.json());
