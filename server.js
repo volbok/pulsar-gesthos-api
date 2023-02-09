967f063475fdce2bfa7fcc60f5c0ddffb9add450
volbok <59297702+volbok@users.noreply.github.com>
2023-02-09 17:19:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -13,9 +13,8 @@ app.use(cors());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Request-Private-Network", "false");
-  res.header("Access-Control-Allow-Private-Network", "true");
+  res.header("Access-Control-Allow-Private-Network", "false");
   res.header(
-    "Access-Control-Allow-Private-Network",
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
