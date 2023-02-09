1867cc04adfa11f0af44afa788f24785940b0837
volbok <59297702+volbok@users.noreply.github.com>
2023-02-09 17:12:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3333;
 app.use(cors());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Request-Private-Network", "true");
+  res.header("Access-Control-Request-Private-Network", "false");
   res.header("Access-Control-Allow-Private-Network", "true");
   res.header(
     "Access-Control-Request-Private-Network",
